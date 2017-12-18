ba6e61428f55a14332ee256c3e65bc1e4c11f822
Batuhan <bapaydin67@gmail.com>
2017-12-18 16:00:53 +0300

    some experiments added on findById controller method

--- src/main/java/com/springboot/testing/rest/VehicleDetailsController.java
+++ src/main/java/com/springboot/testing/rest/VehicleDetailsController.java
@@ -2,9 +2,13 @@ package com.springboot.testing.rest;
 
 import com.springboot.testing.domain.VehicleDetail;
 import com.springboot.testing.service.VehicleDetailsService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.Collection;
+import java.util.Objects;
 
 @RestController
 @RequestMapping(value = "/vehicles")
@@ -28,8 +32,15 @@ public class VehicleDetailsController {
     }
 
     @GetMapping(value = "/{id}")
-    public VehicleDetail findById(@PathVariable(value = "id") Long id) {
-        return vehicleDetailsService.findById(id);
+    public ResponseEntity<Object> findById(@PathVariable(value = "id") Long id) {
+        VehicleDetail byId = vehicleDetailsService.findById(id);
+        if (Objects.isNull(byId)) {
+            return ResponseEntity.noContent().build();
+        }
+
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(byId.getId()).toUri();
+
+        return ResponseEntity.created(location).build();
     }
 
 }
--- src/test/java/com/springboot/testing/rest/VehicleDetailControllerTest.java
+++ src/test/java/com/springboot/testing/rest/VehicleDetailControllerTest.java
@@ -11,12 +11,14 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import java.util.Arrays;
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
 import static org.mockito.BDDMockito.given;
@@ -58,13 +60,23 @@ public class VehicleDetailControllerTest {
     }
 
     @Test
-    public void is_vehicledetails_found_by_id() throws Exception {
+    public void is_vehicledetails_found_sucessfully_by_given_id() throws Exception {
         //given
         VehicleDetail vehicleDetail = new VehicleDetail(1l, "Ford", "Focus");
         given(this.vehicleDetailsService.findById(anyLong())).willReturn(vehicleDetail);
         //when-then
+        MvcResult mvcResult = this.mvc.perform(MockMvcRequestBuilders.get("/vehicles/{id}", 1l)).
+                andExpect(MockMvcResultMatchers.status().is2xxSuccessful()).andReturn();
+        String location = mvcResult.getResponse().getHeader("Location");
+        assertThat(location).isNotEmpty();
+    }
+
+    @Test
+    public void when_vehicledetails_not_found_by_given_id() throws Exception {
+        //given
+        given(this.vehicleDetailsService.findById(anyLong())).willReturn(null);
+        //when-then
         this.mvc.perform(MockMvcRequestBuilders.get("/vehicles/{id}", 1l)).
-                andExpect(MockMvcResultMatchers.status().isOk()).
-                andExpect(MockMvcResultMatchers.jsonPath("$.make", is("Ford")));
+                andExpect(MockMvcResultMatchers.status().is(204));
     }
 }
